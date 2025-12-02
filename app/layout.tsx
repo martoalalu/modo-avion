6a55731b7f0f6e31fd5d2263a33803b063f84936
Martín Alalu <martinalalu@gmail.com>
2025-12-02 18:01:37 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,11 +21,11 @@ export const metadata: Metadata = {
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/logo.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/logo.png',
   },
 }
 
